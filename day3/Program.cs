3c83957a4515796baeaa53ae8d46199b0d7cb1ab
Bereket Legesse <bereketlegesse2@gmail.com>
2023-08-16 13:23:17 +0300

    Palindrome check

---
+++ day3/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program{
+    public static void Main(string [] args){
+
+    }
+    
+    public static bool palindromCheck(string _string){
+        int left = 0;
+        int right = _string.Length - 1;
+
+        while(left < right){
+            if (char.IsLetter(_string[left]) && char.IsLetter(_string[right])){
+                if(char.ToLower(_string[left]) == char.ToLower(_string[right])){
+                    left ++ ;
+                    right -- ;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            else if(char.IsLetter(_string[left])){
+                right -- ;
+            }
+
+            else{
+                left ++ ;
+            }
+        }
+        return true;        
+    }
+}
